109e64e4af33270dd807402f3c220026df38f734
Ｄａｒｋｎｅｓｓ <135982991+darknesslocalhost@users.noreply.github.com>
2023-11-06 19:50:59 +0100

    Update Main.php

--- src/Alerts/Main.php
+++ src/Alerts/Main.php
@@ -9,31 +9,35 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config; 
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
-	public function onEnable(): void {
-		$this->getLogger()->info("Plugin enabled");
-		$this->saveResource("config.yml");
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-	}
-	public function onDisable(): void {
-		$this->getLogger()->info("Plugin disabled");
-		$this->config->save();
+    private $config;
+
+    public function onEnable(): void {
+        $this->getLogger()->info("Plugin enabled");
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("Plugin disabled");
+        $this->config->save();
         $this->getLogger()->info("Config saved");
-	}
-	public function onCommand(CommandSender $sender, Command $cmd, String $label, array $args): bool {
-		if ($cmd->getName() == "alert") {
-			if ($sender->hasPermission("alert.command")) {
-				$wiadomosc = trim(implode(" ", $args));
-				$alertTitle = $this->PluginBase->getConfig("alertTitle");
-				$alertColor = $this->PluginBase->getConfig("alertColor");
-				$messageColor = $this->PluginBase->getConfig("messageColor");
-				foreach ($this->getServer()->getOnlinePlayers() as $p) {
-					$p->sendTitle("$alertColor$alertTitle", "$messageColor$wiadomosc", 0, 20 * 2, 0);
-				}
-			}
-		}
-		return false;
-	}
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+        if ($cmd->getName() == "alert") {
+            if ($sender->hasPermission("alert.command")) {
+                $wiadomosc = trim(implode(" ", $args));
+                $alertTitle = $this->config->get("alertTitle");
+                $alertColor = $this->config->get("alertColor");
+                $messageColor = $this->config->get("messageColor");
+                foreach ($this->getServer()->getOnlinePlayers() as $p) {
+                    $p->sendTitle("$alertColor$alertTitle", "$messageColor$wiadomosc", 0, 20 * 2, 0);
+                }
+            }
+        }
+        return true;
+    }
 }
